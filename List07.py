1b40486cf9b0db48d4d7c19b7e12cf2aae914b71
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-12-12 15:52:35 +0500

    07

--- List07.py
+++ List07.py
@@ -6,4 +6,12 @@ def main(list1):
     Returns:
         list: return answer
     """
-    return
+    a = []
+    for i in list1:
+        if i == 0 :
+            a.append(False)
+        else:
+            a.append(i)
+
+    return a
+print(main([1,1,1,1,0,0,0,1]))
